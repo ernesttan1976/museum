71e1d7f0c20295e05581915214724a8ad6c8d6b8
ernesttan1976 <ernesttan1976@gmail.com>
2023-03-31 17:52:29 +0800

    configured vite to do tree shaking with rollup because the server limit is 240mb

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +10,14 @@ export default defineConfig({
     proxy: {
       "/api": "http://127.0.0.1:3001",
     }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        nodeResolve(),
+        commonjs()
+      ]
+    }
   }
 })
+
