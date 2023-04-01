810e90e93c760b442e7e9170174450abf51dccd9
ernesttan1976 <ernesttan1976@gmail.com>
2023-04-01 12:38:37 +0800

    adjusted the homepage.css responsive adjusted the scroll behaviour of all pages

--- src/components/ExhibitionGrid/ExhibitionGrid.jsx
+++ src/components/ExhibitionGrid/ExhibitionGrid.jsx
@@ -17,7 +17,7 @@ export default function ExhibitionGrid() {
     <>
       <p>{JSON.stringify(exhibition)}</p>
   <h1>{JSON.stringify(exhibition[0]?.exhibitionLocation.address)}</h1>
-      <Grid className="CustomGrid" container spacing={2}>
+      <Grid className="CustomGrid" container spacing={2} direction="row" justifyContent="center" alignItems="center">
         <Grid item xs={12} sm={6} lg={3}>
           <CustomCard />
         </Grid>
