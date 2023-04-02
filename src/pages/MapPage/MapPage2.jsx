c2cb2e53b2ff29319a27156ff3c2e167e7c430ae
Mark Lim <mark.ljr2@gmail.com>
2023-04-02 22:32:38 +0800

    added directions table,explorer switch and drawer

--- src/pages/MapPage/MapPage2.jsx
+++ src/pages/MapPage/MapPage2.jsx
@@ -12,7 +12,11 @@ import SwapVertIcon from '@mui/icons-material/SwapVert';
 import DirectionsWalkRoundedIcon from '@mui/icons-material/DirectionsWalkRounded';
 
 import MapDirectionsCarousel from "../../components/MapDirectionsCarousel/MapDirectionsCarousel"
-import MapComponent from '../../components/MapComponent/MapComponent';
+import MapDirectionsTable from '../../components/MapDirectionsTable/MapDirectionsTable';
+import MapDirectionsExplorer from '../../components/MapDirectionsTable/MapDirectionsExplorer';
+import MapDirectionsDrawer from "../../components/MapDirectionsDrawer/MapDirectionsDrawer";
+
+import Switch from '@mui/material/Switch';
 
 
 
@@ -110,6 +114,25 @@ export default function MapPage2(){
         console.log("Submit: ",formData);
     }
 
+    //Mark's Code
+
+    // const [toggle,SetToggle] = useState(false);
+
+    // const onToggle = () => {
+    //     toggle ? setToggle(false) : setToggle(true);
+    // }
+
+    const [toggle, setToggle] = useState(true);
+
+    const handleSwitchChange = (event) => {
+      toggle ? setToggle(false) : setToggle(true);
+    }
+
+
+    //Slide up Drawer Code 
+
+
+
 
     return (
         
@@ -143,10 +166,16 @@ export default function MapPage2(){
                     </TextField>
                     <Button  className="RightButton" type="submit"><DirectionsWalkRoundedIcon /></Button>
                 </div>
-            </Box>
-            <h1>Map route Carousel Here</h1>
+                <h1>Map route Carousel Here</h1>
             <MapDirectionsCarousel />
             <h1>swipe up menu of directions Here</h1>
+            <MapDirectionsDrawer />
+            <div>
+                <Switch toggle={toggle} onChange={handleSwitchChange}
+                />                
+            </div>
+            {toggle ? <MapDirectionsTable /> : <MapDirectionsExplorer />}
+            </Box>        
         </Box>
     )
 }
