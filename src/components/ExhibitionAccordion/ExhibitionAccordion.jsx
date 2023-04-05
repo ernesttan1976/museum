6e200d09c5458dc55bc28b9d374f268e2812540a
Ida Tang <idatangxinen@gmail.com>
2023-04-05 11:23:29 +0800

    fix homepage formatting(pending)

--- src/components/ExhibitionAccordion/ExhibitionAccordion.jsx
+++ src/components/ExhibitionAccordion/ExhibitionAccordion.jsx
@@ -5,7 +5,10 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ExhibitionGrid from "../ExhibitionGrid/ExhibitionGrid";
-
+import Box from "@mui/material/Box";
+import "./ExhibitionAccordion.css";
+import ExhibitionCard from "../ExhibitionCard/ExhibitionCard";
+import Grid from "@mui/material/Grid";
 export default function ExhibitionAccordion() {
   const floors = [
     "Basement 1",
@@ -19,27 +22,31 @@ export default function ExhibitionAccordion() {
   ];
 
   return (
-    <div>
+    <>
       <Typography variant="h4" color="text.secondary">
         What's On
       </Typography>
-      {floors.map((floor, index) => (
-        <Accordion key={index}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography>{floor}</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <ExhibitionGrid />
-          </AccordionDetails>
-        </Accordion>
-      ))}
-    </div>
+      <Box className="exhibitionAccordionContainer">
+        {floors.map((floor, index) => (
+          <Accordion className="ExhibitionAccordion" key={index}>
+            <AccordionSummary
+              className="AccordionSummary"
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>{floor}</Typography>
+            </AccordionSummary>
+            <AccordionDetails className="AccordionDetails">
+              <Grid container spacing={2}>
+                <Grid item xs={12} md={6} lg={3}>
+                  <ExhibitionCard />
+                </Grid>
+              </Grid>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </Box>
+    </>
   );
 }
-
-
-
