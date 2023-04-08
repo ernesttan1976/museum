65fe26eb361fca589797e8e7358cb67809c5615d
Mark Lim <mark.ljr2@gmail.com>
2023-04-08 12:58:36 +0800

    updated README - mark

--- src/components/MapDirectionsComponent/MapImages.js
+++ src/components/MapDirectionsComponent/MapImages.js
@@ -1,8 +1,8 @@
-import MapA1 from "../..images/maps/MapA1.png";
-import MapA2 from "../..images/maps/MapA2.png";
-import MapB1 from "../..images/maps/MapB1.png";
-import MapB2 from "../..images/maps/MapB2.png";
-import NoRoute from "../..images/maps/NoRoute.png";
+import MapA1 from "../../images/maps/MapA1.png";
+import MapA2 from "../../images/maps/MapA2.png";
+import MapB1 from "../../images/maps/MapB1.png";
+import MapB2 from "../../images/maps/MapB2.png";
+import NoRoute from "../../images/maps/NoRoute.png";
 
 const MAPIMAGES = {
   MapA1,
