9a0e81cd21ca008e8a475afad27346b13b074255
MayMoeOo95 <122252464+MayMoeOo95@users.noreply.github.com>
2023-04-08 14:18:37 +0800

    update

--- src/pages/AuthPage/LogOutMsg.jsx
+++ src/pages/AuthPage/LogOutMsg.jsx
@@ -1,6 +1,6 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import "./LogOutMSG.css";
+import "./LogOutMsg.css";
 
 export default function LogOutMsg({setUser}) {
 
