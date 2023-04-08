b2879cc47cdd8f9b95d8cbe21560c35eb7e38d9a
Ida Tang <idatangxinen@gmail.com>
2023-04-08 18:49:05 +0800

    css styling

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,11 +1,15 @@
 import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom";
+import "./PageNotFound.css";
+import pepe from "../../images/pepe.jpg";
 
 function PageNotFound() {
   return (
     <>
-      <Typography>
-        Page Not Found. Check out homepage <Link to="/">Here</Link>
+      <Typography variant="h4" className="PageNotFoundMsg">
+        Page Not Found. <br></br><br></br>
+        <img src={pepe} /><br></br>
+        <Link to="/">Check out Homepage Here</Link>
       </Typography>
     </>
   );
